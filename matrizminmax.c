2f610b732912ccfd62f59f4e27f2a16235070e2c
Gabriel Toshiaki Hirai Takematu <90941329+GabrielTHirai@users.noreply.github.com>
2022-02-25 23:33:42 -0300

    Update and rename matrizminmax.cpp to matrizminmax.c

--- matrizminmax.c
+++ matrizminmax.c
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <locale>
-using namespace std;
-
+#include <stdio.h>
 void minmax(int matriz[10][10], int linha, int coluna)
 {
     int max = 0, linhaminmax, colunamin, min = matriz[0][0];
@@ -25,20 +22,20 @@ void minmax(int matriz[10][10], int linha, int coluna)
             colunamin = i;
         }
     }
-    cout<<"Elemento: %d\nLinha: %d\tColuna: %d", min, linhaminmax, colunamin;
+    printf("Elemento: %d\nLinha: %d\tColuna: %d", min, linhaminmax, colunamin);
 }
 
 int main()
 {
     int linha, coluna;
-    cin>> &linha, &coluna;
+    scanf("%d%d", &linha, &coluna);
     int matriz[linha][coluna];
 
     for (int i = 0; i < linha; i++)
     {
         for (int j = 0; j < coluna; j++)
         {
-            cin>>"%d">> matriz[i][j];
+            scanf("%d", matriz[i][j]);
         }
     }
     minmax(matriz, linha, coluna);
